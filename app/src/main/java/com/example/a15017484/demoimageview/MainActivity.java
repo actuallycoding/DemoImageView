69eda34bc8aac53c94f05cf682aab5005869a14f
actuallycoding <15017484@myrp.edu.sg>
2017-04-25 10:34:43 +0800

    Added Images.

--- app/src/main/java/com/example/a15017484/demoimageview/MainActivity.java
+++ app/src/main/java/com/example/a15017484/demoimageview/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.a15017484.demoimageview;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    // Declare the ImageView object called ivDay2
+    ImageView ivDay2;
+    ImageView ivDay4;
+    ImageView ivDay5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get the ImageView obj and assign to ivDay2
+        ivDay2 = (ImageView) findViewById(R.id.imageView2);
+        // Set the image
+        ivDay2.setImageResource(R.drawable.day2);
+        ivDay4 = (ImageView)findViewById(R.id.imageView4);
+        ivDay4.setImageResource(R.drawable.day4);
+        ivDay5 = (ImageView)findViewById(R.id.imageView5);
+        ivDay5.setImageResource(R.drawable.day5);
+
     }
 }
